8646114ddd984e14a40be154cc2dc64ffb5d6481
dindacha <106458349+dindacha@users.noreply.github.com>
2023-01-13 20:12:56 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,80 @@
+const STORAGE_KEY = "BOOKSHELF_APP";
+
+let books = [];
+
+// Mengecek apakah browser mendukung local storage
+function isStorageExist() {
+  if (typeof Storage === undefined) {
+    alert("Browser tidak mendukung local storage");
+    return false;
+  }
+  return true;
+}
+
+function saveData() {
+  const parsed = JSON.stringify(books);
+  localStorage.setItem(STORAGE_KEY, parsed);
+  document.dispatchEvent(new Event("ondatasaved"));
+}
+
+// Mengambil seluruh data buku dari local storage dan dikonversi menjadi objek
+function loadDataFromStorage() {
+  const serializedData = localStorage.getItem(STORAGE_KEY);
+
+  let data = JSON.parse(serializedData);
+
+  if (data !== null) books = data;
+
+  document.dispatchEvent(new Event("ondataloaded"));
+}
+
+function updateDataToStorage() {
+  if (isStorageExist()) saveData();
+}
+
+// Menyusun data buku kedalam objek
+function composeBookObject(title, author, year, isCompleted) {
+  return {
+    id: +new Date(),
+    title,
+    author,
+    year,
+    isCompleted,
+  };
+}
+
+// Mencari data buku sesuai Id
+function findBook(bookId) {
+  for (book of books) {
+    if (book.id === bookId) return book;
+  }
+  return null;
+}
+
+// Mencari index buku dari local storage
+function findBookIndex(bookId) {
+  let index = 0;
+  for (book of books) {
+    if (book.id === bookId) return index;
+
+    index++;
+  }
+
+  return -1;
+}
+
+function refreshDataFromBooks() {
+  const listUncompleted = document.getElementById(UNCOMPLETED_READ_ID);
+  let listCompleted = document.getElementById(COMPLETED_READ_ID);
+
+  for (book of books) {
+    const newBook = inputBook(book.title, book.author, book.year, book.isCompleted);
+    newBook[BOOK_ITEM_ID] = book.id;
+
+    if (book.isCompleted) {
+      listCompleted.append(newBook);
+    } else {
+      listUncompleted.append(newBook);
+    }
+  }
+}
